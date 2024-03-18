bfc62708a7eb36e1f39a5bd1939d517acdb22279
Jarosław Piesak <126494806+s28183@users.noreply.github.com>
2024-03-18 15:37:40 +0100

    Add files via upload

---
+++ ProjektKontenery/ProjektKontenery/Containers/LiquidContainer.cs
@@ -0,0 +1,8 @@
+namespace ProjektKontenery;
+
+public class LiquidContainer : ContainerBase
+{
+    public LiquidContainer(double loadMass, double hight, double ownWeight, double depth) : base(loadMass, hight, ownWeight, depth)
+    {
+    }
+}
---
+++ ProjektKontenery/ProjektKontenery/Exception/OverfillException.cs
@@ -0,0 +1,11 @@
+using System.Transactions;
+
+namespace ProjektKontenery;
+
+public class OverfillException : Exception
+{
+    public OverfillException()
+    {
+    Console.WriteLine("Masa podanego ladunku jest wieksza niz pojemnosc kontenera");    
+    }
+}
